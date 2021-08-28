11eea5acb2106aadc3a9b71fd6dc349e5c1314d3
Hannah Malm <hannahemalm616@gmail.com>
2021-08-28 14:35:22 -0400

    commit tree 19da3f51f2acae090b9db7d7e6f43a6b8f57e8f0

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ActiveRecord::Base
+    #name is not blank
+    validates :name, presence: true
+    #email is unique
+    validates :email, uniqueness: true
+    #phone number is 10 digits
+    validates :phone_number, length: { is: 10 }
 end
